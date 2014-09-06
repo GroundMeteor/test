2942afb769518242417c600087b8c9b2bd3ea00c
Morten Henriksen <mh@gi2.dk>
2014-09-06 07:01:45 +0200

    bump 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:test",
-  version: "0.0.2",
+  version: "0.0.3",
   summary: "Run sync tests on multiple clients and the server",
   // git: "https://github.com/GroundMeteor/Meteor-GroundDB.git"
 });
